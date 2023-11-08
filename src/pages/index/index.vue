91fc99f5cc32511c88e3c14a3075c4a3282f3a52
Azure-001 <zliu3485@gmail.com>
2023-11-08 20:28:29 +0800

    登录界面设置

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,8 +5,8 @@
 <template>
   <view class="index">index</view>
   <div class="ellipsis">哪里贵了? 这么多年都是这个价格</div>
-  <div class="flex justify-center">
-    <div class="items-center w-700 h-100 bg-red-500"></div>
+  <div class="flex justify-center h-100 items-center">
+    <div class="w-300 h-80 bg-blue-700"></div>
   </div>
 </template>
 
